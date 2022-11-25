f6ab26afb6c8bbb036bcd7476d82f4c0b8aeaa9e
Dmitry Kolobanov <kolobokd@gmail.com>
2022-11-25 18:34:57 +0200

    Navigation to BestSellers/Books corrected

--- src/test/java/HW_4/Amazon.java
+++ src/test/java/HW_4/Amazon.java
@@ -13,20 +13,20 @@ import java.util.List;
 
 public class Amazon {
     private final By ACCEPT_COOKIES_BTN = By.id("sp-cc-accept");
-    private final By NO_ADDRESS_CHANGE = By.xpath("//span[contains(text(), 'CONTINUE')]/parent::span");
-    private final By PROCEED_TO_BEST_SELLERS = By.linkText("Best Sellers");
-    private final By PROCEED_TO_BOOKS = By.xpath("//div[@id= 'nav-xshop']/a[text()= 'Books']");
-    private final By BOOK_LINK = By.xpath("//a[@class= 'a-link-normal octopus-pc-item-link']");
-    private final By MENU_RATINGS_COUNT = By.xpath("//span[@class ='a-size-mini a-color-tertiary']");
+    private final By NO_ADDRESS_CHANGE = By.xpath(".//input[@data-action-type = 'DISMISS']");
+    private final By MAIN_MENU_LIST_ITEM = By.xpath(".//a[@data-csa-c-content-id  = 'nav_cs_bestsellers']");
+    private final By PROCEED_TO_ITEMS = By.xpath("//div[@role='treeitem']/a[contains(@href, 'bestsellers/books')]");
+    private final By BOOK_LINK = By.xpath(".//a[@class= 'a-link-normal octopus-pc-item-link']");
+    private final By MENU_RATINGS_COUNT = By.xpath(".//span[@class ='a-size-mini a-color-tertiary']");
     private final By BOOKPAGE_RATINGS_COUNT = By.id("acrCustomerReviewText");
-    private final By PROCEED_TO_RATINGS = By.xpath("//div[@id= 'reviews-medley-footer']/div[(@class= 'a-row a-spacing-medium')]/a");
-    private final By MENU_STARS_COUNT = By.xpath("//i[contains(@class, 'a-star-mini')]");
-    private final By BOOKPAGE_STARS_COUNT = By.xpath("//span[(@class= 'a-icon-alt')]/parent::i");
-    private final By REVIEWS_COUNT_DISPLAYED = By.xpath("//div[@id= 'filter-info-section']/div");
-    private final By EVERY_REVIEW_BLOCK = By.xpath("//div[contains(@id,  '-review-card')]");
-    private final By NEXT_PAGE_LINK = By.xpath("//li[@class = 'a-last']/a");
-    private final By NEXT_PAGE_AVAILABLE = By.xpath("//li[@class = 'a-last']");
-    private final By NO_NEXT_PAGE_LINK = By.xpath("//li[@class = 'a-disabled a-last']");
+    private final By PROCEED_TO_RATINGS = By.xpath(".//div[@id= 'reviews-medley-footer']/div[(@class= 'a-row a-spacing-medium')]/a");
+    private final By MENU_STARS_COUNT = By.xpath(".//i[contains(@class, 'a-star-mini')]");
+    private final By BOOKPAGE_STARS_COUNT = By.xpath(".//span[(@class= 'a-icon-alt')]/parent::i");
+    private final By REVIEWS_COUNT_DISPLAYED = By.xpath(".//div[@id= 'filter-info-section']/div");
+    private final By EVERY_REVIEW_BLOCK = By.xpath(".//div[contains(@id,  '-review-card')]");
+    private final By NEXT_PAGE_LINK = By.xpath(".//li[@class = 'a-last']/a");
+    private final By NEXT_PAGE_AVAILABLE = By.xpath(".//li[@class = 'a-last']");
+    private final By NO_NEXT_PAGE_LINK = By.xpath(".//li[@class = 'a-disabled a-last']");
 
     String ratingsInMenu;
     String ratingsOnBookpage;
@@ -55,11 +55,11 @@ public class Amazon {
         wait.until(ExpectedConditions.elementToBeClickable(NO_ADDRESS_CHANGE));
         browser.findElement(NO_ADDRESS_CHANGE).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(PROCEED_TO_BEST_SELLERS));
-        browser.findElement(PROCEED_TO_BEST_SELLERS).click();
+        wait.until(ExpectedConditions.elementToBeClickable(MAIN_MENU_LIST_ITEM));
+        browser.findElement(MAIN_MENU_LIST_ITEM).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(PROCEED_TO_BOOKS));
-        browser.findElement(PROCEED_TO_BOOKS).click();
+        wait.until(ExpectedConditions.elementToBeClickable(PROCEED_TO_ITEMS));
+        browser.findElement(PROCEED_TO_ITEMS).click();
 
         //------------------ Working with book Nr. 4 (in menu) --------------
 
@@ -117,8 +117,8 @@ public class Amazon {
             for (WebElement we : reviewInList) {        //reviews counting on page routine
 
                 reviewsCounted = reviewsCounted + 1;
-//                System.out.println(reviewsCounted);
-//                System.out.println("");
+                System.out.println(reviewsCounted);
+                System.out.println("");
             }
             if (browser.findElements(NO_NEXT_PAGE_LINK).size() == 0) {          //check if last page
                 if (browser.findElements(NEXT_PAGE_AVAILABLE).size() != 0) {     //additional check for 10 reviews only
