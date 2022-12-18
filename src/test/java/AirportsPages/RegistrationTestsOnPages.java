ff8da2d35be07c22ced6decca23a4f5142fdbd59
Dmitry Kolobanov <kolobokd@gmail.com>
2022-12-18 19:06:16 +0200

    Passenger info and checks added

--- src/test/java/AirportsPages/RegistrationTestsOnPages.java
+++ src/test/java/AirportsPages/RegistrationTestsOnPages.java
@@ -2,22 +2,40 @@ package AirportsPages;
 
 import AirportsPages.pages.HomePage;
 import AirportsPages.pages.PassengerInfo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import pageobject.model.Passenger;
 
 public class RegistrationTestsOnPages {
-    private final String URL = "http://www.qaguru.lv:8089/tickets//";
+    private final String URL = "http://www.qaguru.lv:8089/tickets/";
     private final String FROM_AIRPORT = "RIX";
     private final String TO_AIRPORT = "SFO";
-    private final String paxName = "Vasja";
+    private final By BOOK_BTN = By.id("book2");
 
     private BaseFunc baseFunc = new BaseFunc();
 
     @Test
     public void successfulRegistrationTest() {
+        Passenger passenger = new Passenger("Dima","Kolobok", "Hacker", 2,
+                1, 2, "12-05-2018");
+
         baseFunc.openUrl(URL);
         HomePage homePage = new HomePage(baseFunc);
         homePage.selectAirports(FROM_AIRPORT, TO_AIRPORT);
 
         PassengerInfo infoPage = new PassengerInfo(baseFunc);
+        infoPage.fillInPassengerInfo(passenger);
+
+        Assertions.assertEquals(passenger.getFirstName(), infoPage.getPassengerName(), "Wrong name!");
+        Assertions.assertEquals(FROM_AIRPORT, infoPage.getFirstFromAirport(), "Error msg!");
+        Assertions.assertEquals(FROM_AIRPORT, infoPage.getSecondFromAirport(), "Error msg!");
+        Assertions.assertEquals(TO_AIRPORT, infoPage.getFirstToAirport(), "Error msg!");
+        Assertions.assertEquals(TO_AIRPORT, infoPage.getSecondToAirport(), "Error msg!");
+
+        Assertions.assertTrue(infoPage.getPrice().length() > 0, "Error message!");
+
+        baseFunc.click(BOOK_BTN);
+
     }
 }
--- src/test/java/AirportsPages/pages/PassengerInfo.java
+++ src/test/java/AirportsPages/pages/PassengerInfo.java
@@ -1,11 +1,60 @@
 package AirportsPages.pages;
 
 import AirportsPages.BaseFunc;
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.By;
+import pageobject.model.Passenger;
 
 public class PassengerInfo {
+    private final By FIRST_NAME = By.id("name");
+    private final By LAST_NAME = By.id("surname");
+    private final By DISCOUNT = By.id("discount");
+    private final By ADULTS = By.id("adults");
+    private final By CHILDREN = By.id("children");
+    private final By BAG = By.id("bugs");
+    private final By FLIGHT = By.id("flight");
+    private final By GET_PRICE_BTN = By.xpath(".//span[@onclick = 'setLang();']");
+    private final By DATA_ON_FORM = By.xpath(".//span[@class = 'bTxt']");
+    private final By RESPONSE = By.id("response");
     private BaseFunc baseFunc;
 
     public PassengerInfo(BaseFunc baseFunc) {
         this.baseFunc = baseFunc;
+    }
+    public void fillInPassengerInfo(Passenger passenger)   {
+        baseFunc.type(FIRST_NAME, passenger.getFirstName());
+        baseFunc.type(LAST_NAME, passenger.getLastName());
+        baseFunc.type(DISCOUNT, passenger.getDiscount());
+        baseFunc.type(ADULTS, passenger.getPeopleCount());
+        baseFunc.type(CHILDREN, passenger.getChildCount());
+        baseFunc.type(BAG, passenger.getBagCount());
+        baseFunc.selectByText(FLIGHT, passenger.getDate());
+        baseFunc.click(GET_PRICE_BTN);
+        baseFunc.waitForElementsCountToBe(DATA_ON_FORM, 5);
+    }
+    public String getFirstFromAirport() {
+        return baseFunc.findElements(DATA_ON_FORM).get(0).getText();
+    }
+
+    public String getFirstToAirport() {
+        return baseFunc.findElements(DATA_ON_FORM).get(1).getText();
+    }
+
+    public String getSecondFromAirport() {
+        return baseFunc.findElements(DATA_ON_FORM).get(3).getText();
+    }
+
+    public String getSecondToAirport() {
+        return baseFunc.findElements(DATA_ON_FORM).get(4).getText();
+    }
+
+    public String getPassengerName() {
+        String name = baseFunc.findElements(DATA_ON_FORM).get(2).getText(); //Dima!
+        return name.substring(0, name.length() - 1);
+    }
+
+    public String getPrice() {
+        String text = baseFunc.findElement(RESPONSE).getText();
+        return StringUtils.substringBetween(text, "for ", " EUR");
     }
 }
